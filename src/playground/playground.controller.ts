33bdfd59dd809c23cbcfab83cbf43f568ada0eab
stephenhi <stephenhi@pressassociation.com>
2023-05-16 13:58:45 +0100

    feat(added-controller): testing-sonarcloud
    QA should pass.

--- src/playground/playground.controller.ts
+++ src/playground/playground.controller.ts
@@ -5,14 +5,6 @@ export class PlaygroundController {
 
   @Get()
   findAll(): string {
-    let password = '712537273572§53725'; // crap password
-    password = '712537273572§53725'; // crap password
-    return 'This action returns stuff and a crap password ' + password;
-  }
-
-  @Get()
-  sameFindAll(): string {
-    const newPassword = '71637167367637627367'; // crap password
-    return 'This action returns stuff and a crap password ' + newPassword;
+    return 'This action returns stuff' ;
   }
 }
